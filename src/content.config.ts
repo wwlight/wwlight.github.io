36fb3d24ad967c8fe77d525ffb153b342405014f
wwlight <wwlight8@qq.com>
2026-02-27 23:45:16 +0800

    docs: update

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,5 +3,8 @@ import { docsSchema } from '@astrojs/starlight/schema'
 import { defineCollection } from 'astro:content'
 
 export const collections = {
-  docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+  docs: defineCollection({
+    loader: docsLoader(),
+    schema: docsSchema(),
+  }),
 }
